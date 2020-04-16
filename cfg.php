bc47428d78fb54448c01526338d4a5b93d5f690d
Refky Wijaya <priv8refky@gmail.com>
2020-04-16 16:52:00 +0700

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$token = "cookie: DOGEToken=uSdLcsw37Zqtdving7Sk9jxxxxxxxxxx";
+$cookie = "cookie: _gat_gtag_UA_xxxxxxxxx_2=1";
+$cookie1 = "cookie: _ga=GA1.2.471654034.xxxxxxxxxx";
+$cookie2 = "cookie: _gid=GA1.2.1862712830.xxxxxxxxxx";
+$useragent = "user-agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0";
+$phpsessid = "cookie: PHPSESSID=1f7f1511cac159fd40af51xxxxxxxxxx";
+
+
+
+
+?>
